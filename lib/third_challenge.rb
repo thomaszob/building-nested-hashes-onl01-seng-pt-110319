089f222952eeeb9ae939ca683281e0ad0d8939e2
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-19 02:07:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,21 +2,32 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        :name => "Lord Montague",
+        :age => 53
         #your key/value pairs here
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
+        :name => "Romeo",
+        :age => 15,
+        :status => "Alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
+        :name => "Lord Capulet",
+        :age => 53
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Montague",
+        :age => 53
         #your key/value pairs here
         },
       :heroine => {
